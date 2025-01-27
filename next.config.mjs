f1d7668ec53e62e14248108fbea913f4bfee24d6
Seal0220 <eddie920220@gmail.com>
2025-01-28 07:07:36 +0800

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const HOSTS = {
+    LOCAL: 'http://localhost:5000',
+    // SERVER: '/api'
+}
 
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    eslint: {
+        ignoreDuringBuilds: false,
+    },
+    webpack: (config) => {
+        config.ignoreWarnings = [
+            {
+                message: /Failed to parse source map/, // 示例：忽略特定的警告消息
+            },
+        ];
+
+        return config;
+    },
+    env: {
+        API_HOST: HOSTS.LOCAL,
+        LANG: JSON.stringify(['en', 'tw']),
+    },
+};
